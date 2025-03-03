29aebc9415b5454247936992c7a3026c99482d2f
pbmong <pablo15monterog@gmail.com>
2025-03-04 00:32:30 +0100

    Base Implementation

---
+++ main.py
@@ -0,0 +1,229 @@
+from os import listdir
+from os.path import isfile, join
+
+import random
+
+from missions import secundary_missions
+
+import tkinter as tk
+
+class GUI:
+    
+    def __init__(self):
+        self.size = (1800, 900)
+        
+        self.app = tk.Tk()
+        self.app.geometry("1800x900")
+        self.app.config(background="grey")
+        self.app.resizable(False, False)
+
+        self.app.columnconfigure(0, weight=1)
+        self.app.rowconfigure(0, weight=1)
+        
+        self.def_missions_list = list(range(len(secundary_missions.secundary_missions_list)))
+        self.def_missions_1_ind = 0
+        pass
+    
+    def secundary_missions(self):
+        # Styles
+        style_background_color_def = "#99ff99"
+        style_background_color_atc = "#ff9999"
+        style_background_color_gen = "#e0e0e0"
+        
+        style_borders__color_1 = "#204f54"
+        style_font_color_1 = "black"
+        
+        # Fonts
+        font_title = ("Arial", 20)
+        font_missions = ("Arial", 15)
+        
+        # Frames
+        def_frame = tk.Frame(
+            self.app,
+            highlightbackground=style_borders__color_1, 
+            highlightcolor=style_borders__color_1, 
+            highlightthickness=5, 
+            borderwidth=10,
+            width=self.size[0]/2,
+            height=self.size[1],
+            bg=style_background_color_def
+        ).grid(
+            row=0,
+            column=0,
+            
+        )
+        
+        atc_frame = tk.Frame(
+            self.app,
+            highlightbackground=style_borders__color_1, 
+            highlightcolor=style_borders__color_1, 
+            highlightthickness=5, 
+            width=self.size[0]/2, 
+            height=self.size[1], 
+            bg=style_background_color_atc
+        ).grid(
+            row=0,
+            column=1
+        )
+
+        tk.Label(
+            def_frame,
+            text="Defender secundary missions",
+            bg=style_background_color_def,
+            font=font_title,
+            fg=style_font_color_1,
+            highlightbackground=style_borders__color_1, 
+            highlightcolor=style_borders__color_1,
+            justify="center",
+            width=50,
+            height=1
+        ).place(
+            x = 10,
+            y = 30)
+        
+        self.def_mis_1_text = tk.StringVar(self.app)
+        
+        self.def_mis_1_text.set(secundary_missions.secundary_missions_list[0]["description"])
+        
+        tk.Label(
+            def_frame,
+            name="defender mission 1",
+            text="NO MISSION",
+            bg=style_background_color_gen,
+            font = font_missions,
+            fg=style_font_color_1,
+            highlightbackground=style_borders__color_1, 
+            highlightcolor=style_borders__color_1,
+            justify="center",
+            width=77,
+            height=15,
+            textvariable=self.def_mis_1_text
+        ).place( x = 20, y = 150)
+        
+        tk.Button(self.app,text="Generate",command=self.generate_defender_mission_1, font=font_missions).place(x=20,y=100)
+        tk.Button(self.app,text="Discard",command=self.discard_defender_mission_1, font=font_missions).place(x=130,y=100)
+        tk.Button(self.app,text="Clean",command=self.clean_defender_mission_1, font=font_missions).place(x=225,y=100)
+        
+        self.app.mainloop()
+        
+    
+    def generate_defender_mission_1(self):
+        l = len(self.def_missions_list)
+        if l > 0:
+            self.def_missions_1_ind = random.randint(0, l-1)
+            ind = self.def_missions_list[self.def_missions_1_ind]
+            self.def_mis_1_text.set(secundary_missions.secundary_missions_list[ind]["description"])
+        else:
+            self.def_mis_1_text.set("NO MISSION")
+            
+        self.app.mainloop()
+        
+    def discard_defender_mission_1(self):
+        if self.def_missions_1_ind > -1:
+            self.def_missions_list.remove(self.def_missions_list[self.def_missions_1_ind])
+        self.def_missions_1_ind = -1
+        self.clean_defender_mission_1()
+        
+    def clean_defender_mission_1(self):
+        self.def_mis_1_text.set("NO MISSION")
+        self.app.mainloop()
+        
+    def main(self):
+        # Styles
+        style_background_color_1 = "#1cd8ed"
+        style_borders__color_1 = "#204f54"
+        style_font_color_1 = "white"
+
+        # Frames
+        map_frame = tk.Frame(
+            self.app,
+            borderwidth=10,
+            width=self.size[0]/3,
+            height=self.size[1],
+            bg=style_background_color_1
+        ).grid(
+            row=0,
+            column=0,
+            
+        )
+        
+        main_mission_frame = tk.Frame(
+            highlightbackground=style_borders__color_1, 
+            highlightcolor=style_borders__color_1, 
+            highlightthickness=5, 
+            width=self.size[0]/3, 
+            height=self.size[1], 
+            bg=style_background_color_1
+        ).grid(
+            row=0,
+            column=1
+        )
+
+        secundary_missions_frame = tk.Frame(
+            self.app,
+            borderwidth=10,
+            width=self.size[0]/3,
+            height=self.size[1],
+            bg=style_background_color_1
+        ).grid(
+            row=0,
+            column=2
+        )
+
+        title_txt = tk.StringVar(self.app)
+        input_txt = tk.StringVar(self.app)
+
+        tk.Wm.wm_title(self.app, "Warhammer 40k Battle setup")
+
+        # Widgets
+
+        tk.Label(
+            map_frame,
+            text="Tittle",
+            bg=style_background_color_1,
+            fg=style_font_color_1,
+            highlightbackground=style_borders__color_1, 
+            highlightcolor=style_borders__color_1,
+            justify="center",
+            textvariable=title_txt,
+            width=10,
+            height=5
+        ).place(
+            x = 200,
+            y = 300)
+
+        #tk.Entry(            map_frame,            bg="white",            fg="black",            justify="center",            textvariable=input_txt        ).place(                x = 10,                y = 300            )
+
+        deployment_image = tk.PhotoImage(file="deployment/crucible_of_battle.png")
+        self.img_label = tk.Label(
+            map_frame,
+            image=deployment_image
+        )
+        self.img_label.place(
+            x = 500,
+            y = 300
+        )
+
+        onlyfiles = [f for f in listdir("./deployment") if isfile(join("./deployment", f))]
+
+        self.lb = tk.Listbox(self.app, selectmode="single", height = len(onlyfiles), font=("Arial",16)) # create Listbox
+        for x in onlyfiles: self.lb.insert(tk.END, x)
+        self.lb.place(x=600,y=200)
+
+        tk.Button(self.app,text="Start",command=self.printIt, font=("Arial",16)).place(x=730,y=200)
+
+        self.app.mainloop()
+        
+    def printIt(self): 
+        SelectList = self.lb.curselection()
+        selection_list=[self.lb.get(i) for i in SelectList] # this will print the value you select
+        deployment_image = tk.PhotoImage(file="deployment/" + selection_list[0])
+        self.img_label.config(image=deployment_image)
+        self.img_label.place(
+            x = 500,
+            y = 300
+        )
+        self.app.mainloop()
+            
+window = GUI()
+window.secundary_missions()
---
+++ missions/secundary_missions.py
@@ -0,0 +1,96 @@
+
+secundary_missions_list =[
+    {
+        "name":"A Tempting Target",
+        "description":
+            "A TEMPTING TARGET\n\n\
+When this Secondary Mission card is drawn,your opponent must select one objective marker in \n\
+No Man’s Land. At the end of your turn, if you control that selected objective marker, this \n\
+Secondary Mission is achieved and you score 5VP."
+    },
+    {
+        "name":"Capture Enemy Outpost",
+        "description":
+            "CAPTURE ENEMY OUTPOST\n\n\
+At the end of your turn, if you control one or more objective markers in your opponent's\n\
+deployment zone, this Secondary Mission is achieved and you score 8VP."
+    },
+    {
+        "name":"Area Denial",
+        "description":
+            "AREAL DENIAL\n\n\
+At the end of your turn, if one or more units from your army (excluding Battle-shocked units)\n\
+are wholly within 6” of the centre of the battlefield, and there are no enemy units wholly\n\
+within 6” of the centre of the battlefield, this Secondary Mission is achieved and you score 5VP.\n\
+If, at the end of your turn, there are one or more enemy units wholly within 6” of the centre of\n\
+the battlefield, but there are no enemy units within 3” of the centre of the battlefield,\n\
+then this Secondary Mission is still achieved, but in this instance you score 3VP instead of 5VP."
+    },
+    {
+        "name":"Secure No Man’s Land",
+        "description":
+            "SECURE NO MAN´S LAND\n\n\
+At the end of your turn, if you control two or more objective markers in No Man’s Land,\n\
+this Secondary Mission is achieved and you score 5VP. If, at the end of your turn,\n\
+you only control one objective marker in No Man’s Land, this Secondary Mission is\n\
+still achieved, but in this instance you score 2VP instead of 5VP."
+    },
+    {
+        "name":"Overwhelming Force",
+        "description":
+            "OVERWHELMING FORCE\n\n\
+While this Secondary Mission is active, each time an enemy unit that started the\n\
+turn within range of an objective marker is destroyed, you score 3VP\n\
+(to a maximum of 5VP). Note that VP are scored even if such a unit is destroyed\n\
+and then subsequently resurrected for any reason. If you score any VP from this\n\
+Secondary Mission during a turn, then at the end of that turn this Secondary Mission is achieved."
+    },
+    {
+        "name":"Defend Stronghold",
+        "description":
+            "DEFEND STRONGHOLD\n\n\
+At the end of your opponent’s turn, or at the end of the battle (whichever comes first),\n\
+if you control one or more objective markers in your own deployment zone, this Secondary\n\
+Mission is achieved and you score 3VP. This Secondary Mission cannot be achieved during the\n\
+first battle round; if you draw this Secondary Mission card during the first battle round,\n\
+draw a new Secondary Mission card and shuffle this Secondary Mission card back into your\n\
+Secondary Mission deck."
+    },
+    {
+        "name":"Extend Battle Lines",
+        "description":
+            "EXTEND BATTLE LINES \n\n\
+At the end of your turn, if you control one or more objective markers in your own deployment\n\
+zone and you also control one or more objective markers in No Man’s Land, this\n\
+Secondary Mission is achieved and you score 5VP. If you only have one unit remaining\n\
+in your army, then this Secondary Mission is instead achieved at the end of your\n\
+turn if that unit controls one objective marker in No Man’s Land, but in this\n\
+instance you score 2VP instead of 5VP."
+    },
+    {
+        "name":"No Prisoners",
+        "description":
+            "NO PRISIONERS\n\n\
+While this Secondary Mission is active, each time an enemy unit is destroyed, you score 2VP\n\
+(to a maximum of 5VP). Note that VP are scored even if such a unit is destroyed and then\n\
+subsequently resurrected for any reason. If you score any VP from this Secondary Mission\n\
+during a turn, then at the end of that turn this Secondary Mission is achieved."
+    },
+    {
+        "name":"Investigate Signals",
+        "description":
+            "INVESTIGATE SIGNALS\n\n\
+In your Shooting phase, you can select one or more units from your army that are not\n\
+Battle-shocked and are eligible to shoot. Until the end of your turn, the units you\n\
+selected are not eligible to shoot or declare a charge. At the end of your turn, each\n\
+corner of the battlefield that has one or more of these selected units wholly within 9”\n\
+of it is scanned by your army. If one or more corners are scanned by your army, this\n\
+Secondary Mission is achieved and you score 2VP for each corner scanned by your army\n\
+this turn."
+    },
+    {
+        "name":"",
+        "description":
+            "Empty"
+    }
+]
